90d015ef08513b546c01e608d59f995a623a37e6
Samuel Mintah <144267188+mintah275@users.noreply.github.com>
2024-04-22 16:36:11 +0000

    Update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function deleteTask(taskId){
+    fetch('/delete-task', {
+        method : 'POST',
+        body : JSON.stringify({taskId : taskId}),
+    }).then((_res) => {
+        window.location.href = '/';
+    
+    });
+}
